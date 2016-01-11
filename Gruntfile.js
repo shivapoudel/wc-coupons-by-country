f76c769bd0814a873d04248f0420e8a3cbb93391
Shiva Poudel <dev.shivapoudel@gmail.com>
2016-01-11 14:07:12 +0545

    Grunt workflow done

---
+++ Gruntfile.js
@@ -0,0 +1,61 @@
+/* jshint node:true */
+module.exports = function( grunt ){
+	'use strict';
+
+	grunt.initConfig({
+
+		// Generate POT files.
+		makepot: {
+			dist: {
+				options: {
+					type: 'wp-plugin',
+					domainPath: 'languages',
+					potFilename: 'wc-coupons-by-country.pot',
+					potHeaders: {
+						'report-msgid-bugs-to': 'https://github.com/axisthemes/wc-coupons-by-country/issues',
+						'language-team': 'LANGUAGE <EMAIL@ADDRESS>'
+					}
+				}
+			}
+		},
+
+		// Check textdomain errors.
+		checktextdomain: {
+			options: {
+				text_domain: 'wc-coupons-by-country',
+				keywords: [
+					'__:1,2d',
+					'_e:1,2d',
+					'_x:1,2c,3d',
+					'esc_html__:1,2d',
+					'esc_html_e:1,2d',
+					'esc_html_x:1,2c,3d',
+					'esc_attr__:1,2d',
+					'esc_attr_e:1,2d',
+					'esc_attr_x:1,2c,3d',
+					'_ex:1,2c,3d',
+					'_n:1,2,4d',
+					'_nx:1,2,4c,5d',
+					'_n_noop:1,2,3d',
+					'_nx_noop:1,2,3c,4d'
+				]
+			},
+			files: {
+				src: [
+					'**/*.php',
+					'!node_modules/**'
+				],
+				expand: true
+			}
+		}
+	});
+
+	// Load NPM tasks to be used here
+	grunt.loadNpmTasks( 'grunt-wp-i18n' );
+	grunt.loadNpmTasks( 'grunt-checktextdomain' );
+
+	// Register tasks
+	grunt.registerTask( 'default', [
+		'makepot'
+	]);
+};
